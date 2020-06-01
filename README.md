2c20c41577a16780be21ea04d7a2098a282fdee2
Sophia Li <sophiali124@gmail.com>
2020-06-01 12:48:40 -0700

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
+![GitHub top language](https://img.shields.io/github/languages/top/rmorabia/rashee.svg?style=for-the-badge) ![GitHub repo size](https://img.shields.io/github/repo-size/sophi-li/scrape-recipe.svg?style=for-the-badge) ![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/sophi-li/scrape-recipe.svg?style=for-the-badge) ![GitHub](https://img.shields.io/github/license/sophi-li/scrape-recipe.svg?style=for-the-badge)
+
 # Scrape Recipe
 
+Scrape recipe websites with [recipe schema data](https://schema.org/Recipe) using [Playwright](https://github.com/microsoft/playwright).
+
 ## Installation
+
+Using npm:
 
 ```
 npm install scrape-recipe
@@ -10,10 +16,18 @@ npm install scrape-recipe
 
 ```js
 // import module
-const scrapeRecipe = require("scrape-recipe");
+const scrapeRecipe = require('scrape-recipe')
 
 async function getRecipe() {
-  let recipe = await scrapeRecipe("url");
-  console.log(recipe);
+  let recipe = await scrapeRecipe('recipe-website-url')
+  console.log(recipe)
 }
 ```
+
+## Contributing
+
+Issues and pull requests are welcome. This codebase follows the [Contributor Covenant standard](https://github.com/sophi-li/scrape-recipe/blob/master/CODE_OF_CONDUCT.md).
+
+## License
+
+[MIT](https://github.com/sophi-li/scrape-recipe/blob/master/LICENSE)
--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const jsonld = require("jsonld");
-const { chromium } = require("playwright");
+const jsonld = require('jsonld')
+const { chromium } = require('playwright')
 
 const scrapeRecipe = async (url) => {
-  console.log("url: ", url);
+  console.log('url: ', url)
 
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.goto(url);
+  const browser = await chromium.launch()
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.goto(url)
 
-  const handle = await page.$('script[type="application/ld+json"]');
-  const text = await handle.innerText();
+  const handle = await page.$('script[type="application/ld+json"]')
+  const text = await handle.innerText()
 
   const schemaContext = {
-    recipe: "http://schema.org/Recipe",
-    recipeIngredient: "http://schema.org/recipeIngredient",
-  };
-
-  const compacted = await jsonld.compact(JSON.parse(text), schemaContext);
-
-  let ingredients = compacted.recipeIngredient;
-
-  if (Array.isArray(compacted)) {
-    ingredients = compacted["@graph"].filter(
-      (item) => item["@type"] === "recipe"
-    )[0].recipeIngredient;
+    recipe: 'http://schema.org/Recipe',
+    recipeIngredient: 'http://schema.org/recipeIngredient',
   }
 
-  await browser.close();
-  return ingredients;
-};
+  const compacted = await jsonld.compact(JSON.parse(text), schemaContext)
 
-module.exports = scrapeRecipe;
+  let ingredients = compacted.recipeIngredient
+
+  if (Array.isArray(compacted)) {
+    ingredients = compacted['@graph'].filter(
+      (item) => item['@type'] === 'recipe'
+    )[0].recipeIngredient
+  }
+
+  await browser.close()
+  return ingredients
+}
+
+module.exports = scrapeRecipe
